e38a595a31f7293c363300a06192c22492eec274
ConorL11 <conorlamb11@gmail.com>
2023-12-05 07:06:28 -0700

    Reconfigure Server file for Prod

--- backend/server-dev.js
+++ backend/server-dev.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,34 +5,22 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import leagueMemberRoutes from './routes/leagueMemberRoutes.js';
 import cpiDataRoutes from './routes/cpiDataRoutes.js';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 connectDB(); // Connect to MongoDB
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 
 app.use('/api/leagueMembers', leagueMemberRoutes);
 app.use('/api/cpiData', cpiDataRoutes);
 
+
 app.use(notFound);
 app.use(errorHandler);
-
-
-const __dirname = path.resolve();
-if(process.env.NODE_ENV === 'production'){
-    // set static folder
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-    app.get("*", (req,res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running...');
-    });
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

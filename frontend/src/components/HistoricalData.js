b5033bd5733e54b294677381d383204318cd72e6
ConorL11 <74984940+ConorL11@users.noreply.github.com>
2024-01-16 08:29:43 -0700

    Working API Call in Front End

--- frontend/src/components/HistoricalData.js
+++ frontend/src/components/HistoricalData.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+
+
 function HistoricalData(){
+
+    const fetchHistoricalData = async() => {
+        const seasons = await axios.get('/api/seasons');
+        console.log(seasons)
+    }
+
+    useEffect(() => {
+        fetchHistoricalData()
+    }, [])
+    
 
     return (
         <div>
--- frontend/src/pages/HistoryPage.js
+++ frontend/src/pages/HistoryPage.js
@@ -1,67 +1,12 @@
-import { pastWinners } from "../content/constants";
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { pastWinners } from "../content/constants";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 import HistoricalData from "../components/HistoricalData";
 
 function HistoryPage() {
 
-    const [leagueMembers, setLeagueMembers] = useState([]);
-
-    useEffect(() => {
-        const getLeagueMembers = async () => {
-            const { data } = await axios.get('/api/leagueMembers');
-            setLeagueMembers(data);
-        };
-
-        getLeagueMembers();
-    }, []);
-
-    // return (
-    //     <HistoricalData
-    // )
-
     return (
-        <div>
-            <table className="myTable">
-                <caption>League History</caption>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Champion 👑</th>
-                        <th>Loser 🚽</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pastWinners.map(year => (
-                        <tr key={year.year}>
-                            <td>{year.year}</td>
-                            <td data-cell="Champion">{year.winningUser}</td>
-                            <td data-cell="Loser">{year.losingUser}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            <table className="myTable">
-                <caption>League Members</caption>
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>User Name</th>
-                        <th>User Nickname</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {leagueMembers.map(member => (
-                        <tr key={member.user_id}>
-                            <td>{member.user_id}</td>
-                            <td>{member.user_name}</td>
-                            <td>{member.user_nickname}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <HistoricalData/>
     )
 }
 

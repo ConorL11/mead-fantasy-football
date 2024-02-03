bb621e1dca0d907ec606d478db7dd64dbb02e188
ConorL11 <conorlamb11@gmail.com>
2024-02-03 13:10:41 -0700

    more work

--- frontend/src/components/seasonslog/ManagerSummary.js
+++ frontend/src/components/seasonslog/ManagerSummary.js
@@ -2,22 +2,23 @@ import Avatar from "../Avatar";
 
 function ManagerSummary({ manager }){
 
+
+    console.log(manager)
+
     return (
         <div className="insightContainer">
             <Avatar manager={manager}/>
             <h1>{manager.user_name}</h1>
             <div>Seasons: {manager.seasons.length}</div>
-            <div>Championships: {manager.seasons.length}</div>
-            <div>High Scorer: {manager.seasons.length}</div>
-            <div>Biggest Loser: {manager.seasons.length}</div>
+            <div>Championships: {manager.summary.championships}</div>
+            <div>Runner Ups: {manager.summary.runnerUp}</div>
+            <div>Biggest Losers: {manager.summary.biggestLosers}</div>
+            <div>Record: {manager.summary.wins} - {manager.summary.losses}</div>
 
-            <div>Points: {manager.seasons.length}</div>
-            <div>Record: {manager.seasons.length}</div>
-            <div>Average Playoff Seed: {manager.seasons.length}</div>
-            <div>Playoff Appearances: {manager.seasons.length}</div>
 
-            <div>Record: {manager.seasons.length}</div>
-            <div>Total Points: {manager.seasons.length}</div>
+            <div>Points: {manager.summary.points}</div>
+            <div>Points Against: {manager.summary.pointsAgainst}</div>
+
         </div>
     )
 }
--- frontend/src/pages/SeasonsLogPage.js
+++ frontend/src/pages/SeasonsLogPage.js
@@ -48,6 +48,42 @@ function SeasonsLogPage(){
             }
         }
         manager.seasons.sort((a,b) => b.year - a.year);
+
+        //Loop over seasons to get Manager Summary
+        manager.summary = {
+            championships: 0,
+            biggestLosers: 0,
+            runnerUps: 0,
+
+            games: 0,
+            points: 0,
+            pointsAgainst: 0,
+            wins: 0,
+            losses: 0,
+            seedTotal: 0,
+            adds: 0,
+            trades: 0,
+            expectedWins: 0,
+            playoffAppearances: 0,
+        };
+
+        for(const season of manager.seasons){
+            manager.summary.championships += (season.results.championUser === manager._id) ? 1 : 0;
+            manager.summary.runnerUps += (season.results.runnerUpUser === manager._id) ? 1 : 0;
+            manager.summary.biggestLosers += (season.results.losingUser === manager._id) ? 1 : 0;
+            manager.summary.games += (season.team.summary.regularSeason.wins + season.team.summary.regularSeason.losses);
+            manager.summary.points += season.team.summary.regularSeason.points;
+            manager.summary.pointsAgainst += season.team.summary.regularSeason.pointsAgainst;
+            manager.summary.wins += season.team.summary.regularSeason.wins;
+            manager.summary.losses += season.team.summary.regularSeason.losses;
+            manager.summary.seedTotal += season.team.summary.regularSeason.playoffSeed;
+            manager.summary.adds += season.team.transactions.adds;
+            manager.summary.trades += season.team.transactions.trades;
+
+            manager.summary.expectedWins += season.team.summary.regularSeason.expectedWins;
+            manager.summary.playoffAppearances += (season.team.summary.regularSeason.playoffSeed <= 6) ? 1 : 0;
+        }
+
         return manager
     }
 
